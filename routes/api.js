5addc3773fcef8e6df53cef321516be2709d2e7f
workiy-git <narayanabrabu.n@workiy.ca>
2024-02-23 15:28:22 +0530

    updated code

--- routes/api.js
+++ routes/api.js
@@ -1,44 +1,8 @@
-// routes/api.js
-
 const express = require('express');
 const router = express.Router();
-const { LoginModel, UserModel, LandingpageModel } = require('../models/DataModel');
+const dataController = require('../controllers/dataContollers');
 
-router.get('/login', async (req, res) => {
-  try {
-    const data = await LoginModel.find();
-    res.json(data);
-  } catch (error) {
-    console.error(error);
-    res.status(500).send('Server Error');
-  }
-});
-
-router.get('/landingpage', async (req, res) => {
-  try {
-    const data = await LandingpageModel.find();
-    res.json(data);
-  } catch (error) {
-    console.error(error);
-    res.status(500).send('Server Error');
-  }
-});
-
-router.post('/login', async (req, res) => {
-  const { companyName, userName,  password } = req.body;
-
-  try {
-    const user = await UserModel.findOne({ companyName, userName, password });
-
-    if (user) {
-      res.json({ success: true, message: 'Login successful' });
-    } else {
-      res.json({ success: false, message: 'Invalid credentials' });
-    }
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({ success: false, message: 'Server Error' });
-  }
-});
+router.get('/logindata', dataController.getloginData);
+router.get('/headerdata', dataController.getheaderData);
 
 module.exports = router;
