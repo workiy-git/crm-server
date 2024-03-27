7fd384d8c45f886940415ed3213fc910a00fb5e4
workiy-git <narayanabrabu.n@workiy.ca>
2024-03-27 14:58:14 +0530

    init

--- routes/api.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const dataController = require('../controllers/dataContollers');
-
-router.get('/logindata', dataController.getloginData);
-router.get('/headerdata', dataController.getheaderData);
-
-module.exports = router;
