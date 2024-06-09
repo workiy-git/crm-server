df10aeb5389cf0b412e5a183c04c2bb0ab08ab4a
workiy-git <rajesh.k@workiy.ca>
2024-06-09 23:49:24 +0530

    db update

--- connections/Database.js
+++ connections/Database.js
@@ -1,14 +1,16 @@
-const mongoose = require('mongoose');
-const url = 'mongodb://0.0.0.0:27017'; 
-const dbName = 'CRM';
+const mongoose = require("mongoose");
+// const url = 'mongodb://0.0.0.0:27017';
+const url =
+  "mongodb+srv://raguworkiy:Ragu%401978@cluster0.qipjc4f.mongodb.net/crm?retryWrites=true&w=majority";
+const dbName = "CRM";
 
 const connectToDatabase = async () => {
-    try {
-        await mongoose.connect(`${url}/${dbName}`);
-        console.log('Connected to database');
-    } catch (error) {
-        console.error('Error connecting to database:', error);
-    }
+  try {
+    await mongoose.connect(`${url}/${dbName}`);
+    console.log("Connected to database");
+  } catch (error) {
+    console.error("Error connecting to database:", error);
+  }
 };
 
 module.exports = connectToDatabase;
