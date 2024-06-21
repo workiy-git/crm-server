89f4feb711e157e68b47c5d0318f5f937866aaa6
workiy-git <rajesh.k@workiy.ca>
2024-06-21 13:59:07 +0530

    db changes

--- routers/index.js
+++ routers/index.js
@@ -1,12 +1,13 @@
 import express from "express";
 import loginRouter from "./loginRouter.js";
 import menuRouter from "./menuRouter.js";
-import userinfoRouter from "./userinfoRouter.js";
+// import userinfoRouter from "./userinfoRouter.js";
 
 const router = express.Router();
 
 router.use("/login", loginRouter);
+router.use("/pages", loginRouter);
 router.use("/menu", menuRouter);
-router.use("/userinfo", userinfoRouter);
+// router.use("/userinfo", userinfoRouter);
 
 export default router;
