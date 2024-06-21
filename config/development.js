99af635db09b853607438988bb20399cba3a8643
workiy-git <101689935+workiy-git@users.noreply.github.com>
2024-06-21 13:38:53 +0530

    Update development.js for new db

--- config/development.js
+++ config/development.js
@@ -2,7 +2,7 @@
 
 const config = {
   dbUrl:
-    "mongodb+srv://raguworkiy:Ragu%401978@cluster0.qipjc4f.mongodb.net/crm?retryWrites=true&w=majority",
+    "mongodb+srv://raguworkiy:Ragu%401978@cluster0.qipjc4f.mongodb.net/crm-db?retryWrites=true&w=majority",
     loginCollectionName: "Login",
     MenuCollectionName: "Menu", // Add your collection name here
 };
