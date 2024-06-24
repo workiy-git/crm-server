50ca19bc73bab0a61390627c7caec958e9296ec3
NarayanaBrabuN <swastilocalhost@gmail.com>
2024-06-24 17:30:31 +0530

    channel-partner-integration
    data fetch function added

--- routers/channelpartnerRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const { createChannelPartnerData } = require("../controllers/channelpartnerController");
-
-const router = express.Router();
-
-// Route to create new channel partner data
-router.post("/", createChannelPartnerData);
-
-module.exports = router;
