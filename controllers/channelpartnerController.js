4dd10c173b9410cba7e79d45a43781a390a9cc9f
workiy-git <rajesh.k@workiy.ca>
2024-07-18 22:26:48 +0530

    Update channelpartnerController.js

--- controllers/channelpartnerController.js
+++ controllers/channelpartnerController.js
@@ -14,12 +14,9 @@ const createChannelPartnerData = async (req, res) => {
     };
     const collection = getChannelPartnerCollection(req);
     const result = await collection.insertOne(channelPartnerData);
-
+    console.log("channelPartnerData");
+    console.log(result);
     if (result.acknowledged) {
-      res.status(201).json({
-        status: "success",
-        message: "Channel partner data created successfully",
-      });
       console.log("result");
       console.log(result);
 
@@ -29,33 +26,30 @@ const createChannelPartnerData = async (req, res) => {
       const batchFlagsCollection = req.db.collection(
         config.batchFlagsCollectionName
       );
+      const mappingsCollection = req.db.collection(
+        config.mappingsCollectionName
+      );
+      const appDataCollection = req.db.collection(config.appdataCollectionName);
 
       // Prepare the document to insert into batchFlagsCollection
-      const batchFlagDocument = {
-        channelPartnerId: newChannelPartnerId,
-        mapped: "false",
-        createdAt: new Date(), // Timestamp of when the document is created
-        updatedAt: new Date(), // Timestamp of the last update, initially same as createdAt
-        status: "pending", // Initial status of the batch flag
-        processedCount: 0, // Number of times processed, initially 0
-        errors: [], // Array to hold any processing errors
-        // Add any other fields you need for the batchFlagDocument
-      };
-
-      // Insert the new document into batchFlagsCollection
-      const batchFlagResult = await batchFlagsCollection.insertOne(
-        batchFlagDocument
+      const batchFlagResult = await addChannelPartnerToBatchFlagsCollection(
+        batchFlagsCollection,
+        newChannelPartnerId,
+        mappingsCollection,
+        channelPartnerData,
+        appDataCollection
       );
 
-      console.log("batchFlagResult");
-      console.log(batchFlagResult);
-
-      if (batchFlagResult.acknowledged) {
-        console.log(
-          "New ChannelPartnerId successfully added to batchFlagsCollection"
-        );
+      if (batchFlagResult) {
+        res.status(201).json({
+          status: "success",
+          message: "Channel partner data created successfully",
+        });
       } else {
-        console.error("Failed to add ChannelPartnerId to batchFlagsCollection");
+        res.status(500).json({
+          status: "error",
+          message: "Failed to create channel partner data",
+        });
       }
     } else {
       res.status(400).json({
@@ -64,6 +58,7 @@ const createChannelPartnerData = async (req, res) => {
       });
     }
   } catch (error) {
+    console.error("Error:", error);
     res.status(500).json({
       status: "error",
       message: "An error occurred while creating channel partner data",
@@ -95,4 +90,131 @@ const readAllChannelPartnerData = async (req, res) => {
     });
   }
 };
+
+//Functional libraries
+
+async function addChannelPartnerToBatchFlagsCollection(
+  batchFlagsCollection,
+  newChannelPartnerId,
+  mappingsCollection,
+  channelPartnerData,
+  appDataCollection
+) {
+  // Prepare the document to insert into batchFlagsCollection
+  const batchFlagDocument = {
+    channelPartnerId: newChannelPartnerId,
+    mapped: "false",
+    createdAt: new Date(), // Timestamp of when the document is created
+    updatedAt: new Date(), // Timestamp of the last update, initially same as createdAt
+    status: "pending", // Initial status of the batch flag
+    processedCount: 0, // Number of times processed, initially 0
+    errors: [], // Array to hold any processing errors
+    // Add any other fields you need for the batchFlagDocument
+  };
+
+  // Insert the new document into batchFlagsCollection
+  const batchFlagResult = await batchFlagsCollection.insertOne(
+    batchFlagDocument
+  );
+
+  console.log("batchFlagResult");
+  console.log(batchFlagResult);
+
+  if (batchFlagResult.acknowledged) {
+    console.log(
+      "New ChannelPartnerId successfully added to batchFlagsCollection"
+    );
+    console.log("createappDataFromChannelPartnerData");
+    // Assuming this is within an async function
+    const result = await createappDataFromChannelPartnerData(
+      channelPartnerData,
+      mappingsCollection,
+      appDataCollection
+    );
+    if (result) {
+      return true; // Return true if createappDataFromChannelPartnerData succeeded
+    } else {
+      return false; // Optionally handle the failure case
+    }
+  } else {
+    console.error("Failed to add ChannelPartnerId to batchFlagsCollection");
+    return false;
+  }
+}
+
+async function createappDataFromChannelPartnerData(
+  channelPartnerData,
+  mappingsCollection,
+  appDataCollection
+) {
+  // Extract the data you need from the channelPartnerData
+  // Source JSON
+  try {
+    console.log("channelPartnerData");
+    const sourceJSON = channelPartnerData;
+    console.log("sourceJSON");
+    console.log(sourceJSON);
+
+    // Mapping JSON
+    let mappingJSON = {};
+    console.log("mappingJSON");
+    const mappingData = await mappingsCollection
+      .find({ mapping_source: "channelPartner" })
+      .toArray();
+    if (mappingData.length > 0) {
+      mappingJSON = mappingData[0];
+    } else {
+      console.log("No mapping data found for channelPartner");
+    }
+    console.log("mappingJSON");
+    console.log(mappingJSON);
+    // Destination JSON (initially empty or with some default values)
+    let destinationJSON = {};
+
+    // Update process
+    for (const [newKey, oldKey] of Object.entries(mappingJSON)) {
+      if (newKey === "_id") continue; // Skip adding _id to destinationJSON
+
+      if (sourceJSON.hasOwnProperty(oldKey)) {
+        // Special case for dynamic values like new Date()
+        if (newKey === "created_time") {
+          destinationJSON[newKey] = new Date();
+        } else {
+          destinationJSON[newKey] = sourceJSON[oldKey];
+        }
+      } else {
+        // If oldKey is not available in sourceJSON, add newKey with an empty string
+        destinationJSON[newKey] = "";
+      }
+    }
+    // console.log("destinationJSON");
+    // console.log(destinationJSON);
+    // const destinationJSONString = JSON.stringify(destinationJSON);
+    // console.log("destinationJSONString");
+    // console.log(destinationJSONString);
+
+    // let parsedJSON;
+    // try {
+    //   parsedJSON = JSON.parse(destinationJSONString);
+    //   // console.log("parsedJSON");
+    //   // console.log(parsedJSON);
+    // } catch (error) {
+    //   console.log("Error:", error);
+    // }
+    const resultappData = await appDataCollection.insertOne(destinationJSON);
+    console.log("resultappData");
+    console.log(resultappData);
+    if (resultappData.acknowledged) {
+      return true;
+    } else {
+      return false;
+    }
+
+    return true;
+  } catch (error) {
+    console.error("Error:", error);
+    return false; // Indicate failure
+  }
+}
+
 module.exports = { createChannelPartnerData, readAllChannelPartnerData };
