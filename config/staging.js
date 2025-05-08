46d67070d2d03914ed90c38a85704e0a63be5205
workiy-git <101689935+workiy-git@users.noreply.github.com>
2025-05-08 10:18:06 +0530

    Update staging.js

--- config/staging.js
+++ config/staging.js
@@ -1,7 +1,7 @@
 // config/development.js
 
 const config = {
-  dbUrl: "mongodb+srv://crmstag:Workiy2024@workiycrm.9gr0e.mongodb.net/crm-stag?retryWrites=true&w=majority&appName=workiyCRM",
+  dbUrl: "mongodb+srv://raguworkiy:Ragu%401978@cluster0.qipjc4f.mongodb.net/crm-db?retryWrites=true&w=majority",
   MenuCollectionName: "menus", // Add your collection name here
   pagesCollectionName: "pages", // Add your collection name here
   webFormCollectionName: "webforms", // Add your collection name here
